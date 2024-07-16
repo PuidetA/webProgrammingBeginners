4c06770d870457b5fbc2d633329578bb99886b91
PuidetA <aapuidet@gmail.com>
2024-07-16 20:36:55 +0300

    Exercise 4:  Made it so that, if the username in the form already exists in the table, instead of adding the form data to the table, the data for that row is edited instead.

--- w2/basic.js
+++ w2/basic.js
@@ -31,18 +31,38 @@ console.log(squared(9));
 console.log('Done!');
 
 
+//<!--Exercise 2 & 4:-->
 const addNewRow = document.getElementById("submit-data");
 
-addNewRow.addEventListener("click", function() { 
-    const newRowData = document.getElementById("table-body").insertRow();
+addNewRow.addEventListener("click", function() {
+    let usernameFree = true;
+    const tableBody = document.getElementById("table-body");
 
-    newRowData.insertCell(0).innerHTML = document.getElementById("input-username").value;
-    newRowData.insertCell(1).innerHTML = document.getElementById("input-email").value;
-    newRowData.insertCell(2).innerHTML = document.getElementById("input-admin").checked ? "X" : "-";
+
+    //Loop through each row in the table and check if the username value is the same in both cases. 
+    //If it is, then update all fields except the username (since it's the same).
+    for (i=0; i<document.getElementById("table-body").rows.length; i++) {
+        if (document.getElementById("table-body").rows[i].cells[0].innerHTML == document.getElementById("input-username").value) {
+            tableBody.rows[i].cells[1].innerHTML = document.getElementById("input-email").value;
+            tableBody.rows[i].cells[2].innerHTML = document.getElementById("input-admin").checked ? "X" : "-";
+            usernameFree = false; //Make it so that it doesn't add a new row.
+            break;
+        }}
+
+    if (usernameFree) {
+        const newRowData = document.getElementById("table-body").insertRow();
+
+        newRowData.insertCell(0).innerHTML = document.getElementById("input-username").value;
+        newRowData.insertCell(1).innerHTML = document.getElementById("input-email").value;
+        newRowData.insertCell(2).innerHTML = document.getElementById("input-admin").checked ? "X" : "-";
+    }
+    
 
 
 });
 
+
+//<!--Exercise 3:-->
 const wipeDataButton = document.getElementById("empty-table");
 
 wipeDataButton.addEventListener("click", function() {
