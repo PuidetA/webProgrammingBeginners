344df642ca2b4d542f8a9023d6882d15be98aaf3
PuidetA <aapuidet@gmail.com>
2024-07-16 20:14:13 +0300

    Exercise 2: Added input option to the table with inputs and a button.

--- w2/basic.js
+++ w2/basic.js
@@ -29,3 +29,18 @@ console.log(sum3(5, 4));
 console.log(squared(9));
 
 console.log('Done!');
+
+
+const addNewRow = document.getElementById("submit-data");
+
+addNewRow.addEventListener("click", function() { 
+    const newRowData = document.getElementById("table-body").insertRow();
+
+    newRowData.insertCell(0).innerHTML = document.getElementById("input-username").value;
+    newRowData.insertCell(1).innerHTML = document.getElementById("input-email").value;
+    newRowData.insertCell(2).innerHTML = document.getElementById("input-admin").checked ? "X" : "-";
+
+    document.getElementById("input-username").value = "";
+    document.getElementById("input-email").value = "";
+    document.getElementById("input-admin").checked = false;
+});
