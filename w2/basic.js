5f047c9cdd4e6a2f2a15f5daf387d0c7e348fff6
PuidetA <aapuidet@gmail.com>
2024-07-16 21:11:19 +0300

    Exercise 3: Corrected it and made it empty the entire table and not the input fields.
    Exercise 5: Added an addImage function to be used in addNewRow (or edit)

--- w2/basic.js
+++ w2/basic.js
@@ -37,6 +37,7 @@ const addNewRow = document.getElementById("submit-data");
 addNewRow.addEventListener("click", function() {
     let usernameFree = true;
     const tableBody = document.getElementById("table-body");
+    const image = document.getElementById("input-image").files[0];
 
 
     //Loop through each row in the table and check if the username value is the same in both cases. 
@@ -45,6 +46,13 @@ addNewRow.addEventListener("click", function() {
         if (document.getElementById("table-body").rows[i].cells[0].innerHTML == document.getElementById("input-username").value) {
             tableBody.rows[i].cells[1].innerHTML = document.getElementById("input-email").value;
             tableBody.rows[i].cells[2].innerHTML = document.getElementById("input-admin").checked ? "X" : "-";
+
+            //Clear the potential image. The purpose of this part of the function originally was to edit existing rows.
+            //This means that you replace the admin priviledge or the email. So should the image be replaced as well by this logic.
+            tableBody.rows[i].cells[3].innerHTML = ""; 
+            addImage(tableBody.rows[i].cells[3], image);
+
+
             usernameFree = false; //Make it so that it doesn't add a new row.
             break;
         }}
@@ -55,6 +63,7 @@ addNewRow.addEventListener("click", function() {
         newRowData.insertCell(0).innerHTML = document.getElementById("input-username").value;
         newRowData.insertCell(1).innerHTML = document.getElementById("input-email").value;
         newRowData.insertCell(2).innerHTML = document.getElementById("input-admin").checked ? "X" : "-";
+        addImage(newRowData.insertCell(3), image);
     }
     
 
@@ -63,10 +72,30 @@ addNewRow.addEventListener("click", function() {
 
 
 //<!--Exercise 3:-->
-const wipeDataButton = document.getElementById("empty-table");
 
+
+const wipeDataButton = document.getElementById("empty-table");
+/*
 wipeDataButton.addEventListener("click", function() {
     document.getElementById("input-username").value = "";
     document.getElementById("input-email").value = "";
     document.getElementById("input-admin").checked = false;
+});*/
+wipeDataButton.addEventListener("click", function() {
+    for (i=0; i<document.getElementById("table-body").rows.length; i++) {
+        document.getElementById("table-body").deleteRow(i);
+        i--; //go back one step to not skip a row.
+    }
 });
+
+//<!--Exercise 5:--> This adds an image to the table. It's a separate function to make it more readable.
+const image = document.getElementById("input-image");
+
+let addImage = function(cell, image) {
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(image);
+    img.height = 64;
+    img.width = 64;
+    cell.innerHTML = ""; //Clear it first, so we don't have multiple images later on.
+    cell.appendChild(img);
+};
