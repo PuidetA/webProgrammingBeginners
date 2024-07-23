7a7e79deea1b830d2b1c6b2625fe5efbe99caf11
PuidetA <aapuidet@gmail.com>
2024-07-23 17:10:38 +0300

    Exercise 2: add tooltips to the map

--- w5/basic.js
+++ w5/basic.js
@@ -2,7 +2,7 @@ async function fetchAndCreateMap() {
     // #### FETCHING DATA ####
     // fetch geojson data
     const fetchingGeo = await fetch('https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326')
-    const dataGeo = await fetching.json()
+    const dataGeo = await fetchingGeo.json()
     //fetch statfin
     //const fetchingStatPositive = await fetch('https://statfin.stat.fi/PxWeb/sq/4bb2c735-1dc3-4c5e-bde7-2165df85e65f')
     //const dataStatPositive = await fetchingStatPositive.json()
@@ -10,18 +10,28 @@ async function fetchAndCreateMap() {
     //const fetchingStatNegative = await fetch('https://statfin.stat.fi/PxWeb/sq/944493ca-ea4d-4fd9-a75c-4975192f7b6e')
     //const dataStatNegative = await fetchingStatNegative.json()
 
+    // #### ADDING FEATURES ####
+    // this comes before making the map to initalize everything first
+    // Add tooltip with municipality names via leaflet
+    // SOURCE: https://leafletjs.com/reference.html#geojson-oneachfeature
+    const addTooltip = (feature, layer) => {
+        layer.bindTooltip(feature.properties.nimi)
+    };
 
+    
     // #### CREATING MAP ####
     let map = L.map('map', {
         minZoom: -3
     })
 
     let geoJsonL = L.geoJSON(dataGeo, {
-        weight: 2
+        weight: 2,
+        onEachFeature: addTooltip
     }).addTo(map)
 
     map.fitBounds(geoJsonL.getBounds())
 
+    
 
 }
 
