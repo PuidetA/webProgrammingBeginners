1623d11499e61fd63db45b9ff2d3aed84e2b58e5
PuidetA <aapuidet@gmail.com>
2024-07-25 14:01:09 +0300

    connected html to css and js files.
    Exercise 2: Made the div and made the chart with an id=#chart.

--- w6/basic.js
+++ w6/basic.js
@@ -63,7 +63,16 @@ fetch("https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12d
 }
 );
 
-
-
-
-
+const chart = new frappe.Chart("#chart", {
+    height: 450,
+    type: "line",
+    colors: ['#eb5146'],
+    x_labels: [
+        '2000', '2001', '2002', '2003', '2004', 
+        '2005', '2006', '2007', '2008', '2009', 
+        '2010', '2011', '2012', '2013', '2014', 
+        '2015', '2016', '2017', '2018', '2019', 
+        '2020', '2021'
+    ],
+    title: "Title here"
+});
