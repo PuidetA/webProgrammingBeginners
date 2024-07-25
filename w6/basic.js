45272174086d34f25b39b7cb2721ab82efab470c
PuidetA <aapuidet@gmail.com>
2024-07-25 20:54:01 +0300

    Exercises 1 and 2: made the chart work, reworked JS completely.

--- w6/basic.js
+++ w6/basic.js
@@ -1,78 +1,74 @@
+const YEARS = [
+    '2000', '2001', '2002', '2003', '2004', 
+    '2005', '2006', '2007', '2008', '2009', 
+    '2010', '2011', '2012', '2013', '2014', 
+    '2015', '2016', '2017', '2018', '2019', 
+    '2020', '2021'
+];
 
-
-
-//source used: https://www.freecodecamp.org/news/javascript-post-request-how-to-send-an-http-post-request-in-js/
-fetch("https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px", {
-    method: "POST",
-    body: JSON.stringify(
-        {
-        "query": [
-            {
-                "code": "Vuosi",
-                "selection":{
-                    "filter":"item",
-                    "values":[
-                        "2000",
-                        "2001",
-                        "2002",
-                        "2003",
-                        "2004",
-                        "2005",
-                        "2006",
-                        "2007",
-                        "2008",
-                        "2009",
-                        "2010",
-                        "2011",
-                        "2012",
-                        "2013",
-                        "2014",
-                        "2015",
-                        "2016",
-                        "2017",
-                        "2018",
-                        "2019",
-                        "2020",
-                        "2021"
-                    ]
-                }
+async function fetchPopulationData() {
+    const response = await fetch("https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
         },
-        {
-            "code": "Alue",
-            "selection":{
-                "filter":"item",
-            "values":[
-                "SSS"
-            ]
-        }
-    },
-    {
-        "code": "Tiedot",
-        "selection": {
-            "filter": "item",
-            "values": [
-                "vaesto"
-            ]
-        }}
-    ],
-    "response": {
-        "format": "json-stat2"
-    }
-}
-)
-}
-);
+        body: JSON.stringify({
+            query: [
+                {
+                    code: "Vuosi",
+                    selection: {
+                        filter: "item",
+                        values: YEARS
+                    }
+                },
+                {
+                    code: "Alue",
+                    selection: {
+                        filter: "item",
+                        values: ["SSS"]
+                    }
+                },
+                {
+                    code: "Tiedot",
+                    selection: {
+                        filter: "item",
+                        values: ["vaesto"]
+                    }
+                }
+            ],
+            response: {
+                format: "json-stat2"
+            }
+        })
+    });
+    const data = await response.json();
+    return data.value;
+};
 
-const chart = new frappe.Chart("#chart", {
-    height: 450,
-    type: "line",
-    colors: ['#eb5146'],
-    x_labels: [
-        '2000', '2001', '2002', '2003', '2004', 
-        '2005', '2006', '2007', '2008', '2009', 
-        '2010', '2011', '2012', '2013', '2014', 
-        '2015', '2016', '2017', '2018', '2019', 
-        '2020', '2021'
-    ],
-    title: "Title here"
-});
+async function fetchMunicipalityCodes() {
+
+};
+
+async function fetchMunicipalityNames() {
+
+};
+
+
+let chart; // I asked advice from a friend about how to handle the chart object being returned,
+// they asked "Why not just declare it beforehand?" and I decided to use that as an inspiration for this.
+async function makeChart() {
+    const data = await fetchPopulationData();
+    chart = new frappe.Chart("#chart", {
+        height: 450,
+        type: "line",
+        colors: ['#eb5146'],
+        data: {
+            labels: YEARS,
+            datasets: [{values: data}]
+        },
+        title: "Title here"
+    })
+};
+
+
+makeChart();
